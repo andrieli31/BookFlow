a69927b884b2fe462a4cd986145f168995ebfd95
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-04-11 14:11:55 -0300

    Get e set

--- src/modelo/Livro.java
+++ src/modelo/Livro.java
@@ -5,4 +5,29 @@ public class Livro {
 	private int isbn;
 	private String titulo;
 	private String editora;
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
 }
