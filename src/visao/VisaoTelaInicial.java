73d546c6763226f083d20a08e4c2b71fb3a68a82
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-05-15 22:22:34 -0300

    Final telas/2

--- src/visao/VisaoTelaInicial.java
+++ src/visao/VisaoTelaInicial.java
@@ -14,6 +14,9 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
 public class VisaoTelaInicial extends JFrame {
 
@@ -40,46 +43,61 @@ public class VisaoTelaInicial extends JFrame {
 	 * Create the frame.
 	 */
 	public VisaoTelaInicial() {
-		setBackground(new Color(64, 128, 128));
+		setBackground(new Color(0, 64, 128));
 		setForeground(new Color(0, 0, 0));
-		setTitle("Tela inicial");
+		setTitle("TELA INICIAL");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setBounds(100, 100, 941, 870);
+		setBounds(100, 100, 1131, 1143);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(64, 128, 128));
-		contentPane.setForeground(new Color(64, 128, 128));
+		contentPane.setBackground(new Color(0, 64, 128));
+		contentPane.setForeground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[183px][4px][183px][][][][][grow]", "[20px][25px][23px][][][][][][][][][][grow]"));
 		
 		JLabel lblEscolherOpcao = new JLabel("Escolha uma opção:");
+		lblEscolherOpcao.setBounds(91, 107, 170, 24);
 		lblEscolherOpcao.setForeground(new Color(255, 255, 255));
-		lblEscolherOpcao.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		contentPane.add(lblEscolherOpcao, "cell 5 10 2 1,alignx center,aligny top");
+		lblEscolherOpcao.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		
-		JButton btnGerenciaLivro = new JButton("Gerenciamento de livros");
-		btnGerenciaLivro.setBackground(new Color(255, 255, 255));
-		btnGerenciaLivro.setForeground(new Color(64, 128, 128));
-		btnGerenciaLivro.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		contentPane.add(btnGerenciaLivro, "cell 5 11,alignx left,aligny top");
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(170, 229, 256, 256);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\mende\\Downloads\\user-interface (1).png"));
 		
-		JButton btnSair = new JButton("Finalizar");
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		JButton btnNewButton = new JButton("Gerenciar Usuários");
+		btnNewButton.setBounds(170, 508, 256, 29);
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		btnNewButton.setForeground(new Color(0, 64, 128));
 		
-		JButton btnGerenciaUsuario = new JButton("Gerenciamento de livros");
-		btnGerenciaUsuario.setBackground(new Color(255, 255, 255));
-		btnGerenciaUsuario.setForeground(new Color(64, 128, 128));
-		btnGerenciaUsuario.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		contentPane.add(btnGerenciaUsuario, "cell 6 11,alignx left,aligny top");
-		btnSair.setBackground(new Color(255, 255, 255));
-		btnSair.setForeground(new Color(128, 128, 128));
-		btnSair.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		contentPane.add(btnSair, "cell 5 12 2 1,alignx center,aligny top");
+		JLabel lblNewLabel_2 = new JLabel("Bem Vindo!");
+		lblNewLabel_2.setBounds(81, 32, 417, 65);
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 56));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(681, 229, 256, 256);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\mende\\Downloads\\livro.png"));
+		contentPane.add(lblNewLabel);
+		contentPane.add(btnNewButton);
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton_1 = new JButton("Gerenciar Livros");
+		btnNewButton_1.setBounds(708, 508, 222, 29);
+		btnNewButton_1.setBackground(new Color(255, 255, 255));
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		btnNewButton_1.setForeground(new Color(0, 64, 128));
+		contentPane.add(btnNewButton_1);
+		contentPane.add(lblEscolherOpcao);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Já possui uma conta?");
+		lblNewLabel_3.setBounds(1020, 10, 111, 15);
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		contentPane.add(lblNewLabel_3);
 	}
 
 }
