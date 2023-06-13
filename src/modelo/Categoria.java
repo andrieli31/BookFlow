f12d1c0a628049c7898458395ff4918e2733e2b7
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-06-13 17:41:30 -0300

    classe categoria - novo crud

---
+++ src/modelo/Categoria.java
@@ -0,0 +1,42 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Categoria {
+	private int idCategoria;
+	private String nomeCategoria;
+	private String descricao;
+	private Date dataCriacao;
+	private Date dataUltimaUpdate;
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataUltimaUpdate() {
+		return dataUltimaUpdate;
+	}
+	public void setDataUltimaUpdate(Date dataUltimaUpdate) {
+		this.dataUltimaUpdate = dataUltimaUpdate;
+	}
+
+}
