f7266b419fe38fff984898a221ef4e91c215e6b8
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-08-23 19:04:15 -0300

    script banco completo

--- src/resources/script_banco.sql
+++ src/resources/script_banco.sql
@@ -1,13 +1,59 @@
-DROP DATABASE IF EXISTS book_flow;
+DROP DATABASE IF EXISTS bookflow;
 
-CREATE SCHEMA IF NOT EXISTS ´book_flow´;
+CREATE DATABASE IF NOT EXISTS bookflow;
 
-USE book_flow;
+USE bookflow;
 
--- base p/ tabelas!!!!!!!!!!!!
-CREATE TABLE IF NOT EXISTS `pessoa` (
-  `id_pessoa` INT NOT NULL,
-  `primeiro_nome` VARCHAR(45) NOT NULL, 
-  `idade` INT NOT NULL,
-  PRIMARY KEY (`id_pessoa`)
+CREATE TABLE IF NOT EXISTS `livro` (
+ `isbn` BIGINT NOT NULL,
+ `titulo` VARCHAR (60) NOT NULL,
+ `editora` VARCHAR (60) NOT NULL, 
+ `nrEdicao` INT NOT NULL,
+ `anoLancamento` INT NOT NULL,
+ `autor` VARCHAR (60) NOT NULL,
+ 
+PRIMARY KEY (`isbn`)
+ 
 );
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id` BIGINT NOT NULL,
+`login` VARCHAR (45) NOT NULL,
+`senha` VARCHAR (45) NOT NULL,
+
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `autor`(
+`id` BIGINT NOT NULL,
+`nome` VARCHAR (60) NOT NULL,
+`sobrenome` VARCHAR (60) NOT NULL,
+
+PRIMARY KEY (`id`)
+
+);
+
+CREATE TABLE IF NOT EXISTS `pessoa` (
+`cpf` BIGINT NOT NULL,
+`nome` VARCHAR (60) NOT NULL,
+`sobrenome` VARCHAR (60) NOT NULL,
+
+PRIMARY KEY (`cpf`)
+);
+
+CREATE TABLE IF NOT EXISTS `categoria`(
+`idCategoria` INT NOT NULL,
+`idioma` VARCHAR(45) NOT NULL,
+`quantPaginas` INT NOT NULL,
+`genero` VARCHAR (45) NOT NULL,
+
+PRIMARY KEY (`idCategoria`)
+);
+
+INSERT INTO livro (isbn, titulo, editora, nrEdicao, anoLancamento, autor) VALUES ('978853330227', 'aumosso acima de tudo', 'ifsc','56', '2023', 'andrieli mendes');
+INSERT INTO livro (isbn, titulo, editora, nrEdicao, anoLancamento, autor) VALUES ('598814475656', 'gripe', 'ifc', '48', '2022', 'patricia mendes');
+
+INSERT INTO categoria (idCategoria, idioma, quantPaginas, genero) VALUES ('2', 'ingles', '265', 'drama');
+
+SELECT*FROM livro;
+SELECT*FROM categoria;
