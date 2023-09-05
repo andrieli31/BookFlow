12182ef334f15f6dc34a469a35316b694a899564
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-09-05 09:09:53 -0300

    inserir livro

--- src/controle/LivroDAO.java
+++ src/controle/LivroDAO.java
@@ -1,75 +1,44 @@
 package controle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import modelo.ILivroDAO;
 import modelo.Livro;
 
-public class LivroDAO implements ILivroDAO {
+public class LivroDAO {
 
-	private static ArrayList<Livro> tabelaLivros;
-	private static LivroDAO instancia;
-
-	private LivroDAO() {
-
-	}
-
-	public static LivroDAO getInstancia() {
-
-		if (instancia == null) {
-			instancia = new LivroDAO();
-			tabelaLivros = new ArrayList<>();
+	public boolean inserir (Livro l) {
+		
+		Conexao c = Conexao.getInstancia();
+		
+		Connection con = c.conectar();
+		
+		String query = "INSERT INTO livro (isbn, titulo, editora, nrEdicao, anoLancamento, autor) VALUES ('?', '?', '?', '?', '?', '?');";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1, Long.toString(l.getIsbn()));
+			ps.setString(2, l.getTitulo());
+			ps.setString(3, l.getEditora());
+			ps.setString(4, Integer.toString(l.getNrEdicao()));
+			ps.setString(5, Integer.toString(l.getAnoLancamento()));
+			ps.setString(6, l.getAutor());
+			
+			//conslida a execução do comando no banco
+			ps.executeUpdate();
+			
+			//fecha conexao
+			c.fecharConexao();
+		}catch (SQLException l1){
+			
+			l1.printStackTrace();
+			return false;
 		}
-		return instancia;
+		return true;
+		
+		
 	}
-
-	@Override
-	public boolean cadastrarLivro(Livro l) {
-		if (l != null) {
-			tabelaLivros.add(l);
-			return true;
-		}
-
-		return false;
-	}
-
-	public boolean alterarLivro(Livro l) {
-		for (Livro livro : tabelaLivros) {
-			if (livro.getIsbn() == l.getIsbn()) {
-				livro.setEditora(l.getEditora());
-				livro.setNrEdicao(l.getNrEdicao());
-				livro.setTitulo(l.getTitulo());
-				livro.setAnoLancamento(l.getAnoLancamento());
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	public Livro buscarLivroPorIsbn(long isbn) {
-		for (Livro livro : tabelaLivros) {
-			if (livro.getIsbn() == isbn) {
-				return livro;
-			}
-		}
-
-		return null;
-	}
-
-	public boolean deletarLivro(Livro l, Long isbn) {
-
-		for (Livro livro : tabelaLivros) {
-			if (livro.getIsbn() == isbn) {
-				tabelaLivros.remove(livro);
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public ArrayList<Livro> listarLivros() {
-		return tabelaLivros;
-	}
-
 }
